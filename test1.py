3c490b2c8b5a7a68ac48c96aadac0a15f5d7b25c
Alexander Garcia <110160255+alex-m-g@users.noreply.github.com>
2024-10-07 10:50:38 -0500

    Create test1.py
    detect the error from the main python file.

---
+++ test1.py
@@ -0,0 +1,48 @@
+import os
+import pandas as pd
+
+def data_wrangle(folder):
+    files = []
+    for file in os.listdir(folder):
+        file_path = os.path.join(folder, file)
+        files.append(file_path)
+
+    df_test = pd.read_csv(files[0])
+    df_test_beneficiary_data = pd.read_csv(files[1])
+    df_train_inpatient_data = pd.read_csv(files[2])
+    df_train_outpatient_data = pd.read_csv(files[3])
+    df_train = pd.read_csv(files[4])
+    df_train_beneficiary_data = pd.read_csv(files[5])
+    df_train_inpatient_data = pd.read_csv(files[6])
+    df_train_outpatient_data = pd.read_csv(files[7])
+
+    print("Successful variable assignment")
+
+    
+    # replace 2 to 0 for the chronic conditions to indicate False.
+    df_train_beneficiary_data = df_train_beneficiary_data.replace({'ChronicCond_Alzheimer': 2, 'ChronicCond_Heartfailure': 2, 'ChronicCond_KidneyDisease': 2,
+                               'ChronicCond_Cancer': 2, 'ChronicCond_ObstrPulmonary': 2, 'ChronicCond_Depression': 2, 
+                               'ChronicCond_Diabetes': 2, 'ChronicCond_IschemicHeart': 2, 'ChronicCond_Osteoporasis': 2, 
+                               'ChronicCond_rheumatoidarthritis': 2, 'ChronicCond_stroke': 2 }, 0)
+    
+    print("Successful df_train_benificiary_data reassignment Part 1")
+
+    # For RenalDiseaseIndicator replacing 'Y' with 1
+    df_train_beneficiary_data = df_train_beneficiary_data.replace({'RenalDiseaseIndicator': 'Y'}, 1)
+    
+    print("Successful df_train_benificiary_data reassignment Part 2")
+
+    # convert all these columns datatypes to numeric
+    df_train_beneficiary_data[["ChronicCond_Alzheimer", "ChronicCond_Heartfailure", "ChronicCond_KidneyDisease", "ChronicCond_Cancer", "ChronicCond_ObstrPulmonary", 
+                               "ChronicCond_Depression", "ChronicCond_Diabetes", "ChronicCond_IschemicHeart", "ChronicCond_Osteoporasis", "ChronicCond_rheumatoidarthritis", "ChronicCond_stroke", 
+                               "RenalDiseaseIndicator"]] =df_train_beneficiary_data[["ChronicCond_Alzheimer", "ChronicCond_Heartfailure", "ChronicCond_KidneyDisease", 
+                                                                                    "ChronicCond_Cancer", "ChronicCond_ObstrPulmonary", "ChronicCond_Depression", "ChronicCond_Diabetes", "ChronicCond_IschemicHeart", 
+                                                                                    "ChronicCond_Osteoporasis", "ChronicCond_rheumatoidarthritis", "ChronicCond_stroke", "RenalDiseaseIndicator"]].apply(pd.to_numeric)
+    print("Successful conversion of df_train_benificiary_data to numeric")
+
+def main():
+    folder = "archive"
+    data_wrangle(folder)
+
+if __name__ == "__main__":
+    main()
